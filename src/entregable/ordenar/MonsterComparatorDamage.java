d74d5626b7f859200e8b25ab18dda44339f2dca3
Salvador <salvador.karakachoff@gmail.com>
2023-12-16 20:00:33 -0300

    Revert "klkm"
    This reverts commit ca044dd63faf4c2a661867749eec50a78ee21885.

--- src/entregable/ordenar/MonsterComparatorDamage.java
+++
@@ -1,9 +0,0 @@
-package entregable.ordenar;
-
-import game.components.Monster;
-public class MonsterComparatorDamage implements java.util.Comparator<Monster> {
-	@Override
-	public int compare(Monster monsterA, Monster monsterB) {
-		return (monsterA.getDamage()-monsterB.getDamage());
-	}
-}
