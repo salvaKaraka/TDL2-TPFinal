ca044dd63faf4c2a661867749eec50a78ee21885
Salvador <salvador.karakachoff@gmail.com>
2023-12-16 20:00:24 -0300

    klkm

---
+++ src/entregable/excepciones/DrawException.java
@@ -0,0 +1,25 @@
+package entregable.excepciones;
+
+public class DrawException extends Exception {
+
+	public DrawException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DrawException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DrawException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DrawException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/entregable/ordenar/ordenar.java
@@ -0,0 +1,41 @@
+package entregable.ordenar;
+
+import java.util.List;
+import java.util.Collection;
+import java.util.Collections;
+
+import game.components.Monster;
+
+public  class ordenar {
+	
+	public static List<Monster> ordenamientoPorVida(List<Monster> monstruos){
+		   for (int i=0;i<monstruos.size();i++) {
+			      
+			      System.out.println(monstruos.get(i).getLife());
+			    }
+		
+		Collections.sort(monstruos, new MonsterComparatorVida());
+		System.out.println("---------Lista Ordenada-------------");
+		 for (int i=0;i<monstruos.size();i++) {
+		      
+		      System.out.println(monstruos.get(i).getLife());
+		    }
+		
+		return monstruos;
+	}
+	public static List<Monster> ordenamientoPorDamage(List<Monster> monstruos){
+		   for (int i=0;i<monstruos.size();i++) {
+			      
+			      System.out.println(monstruos.get(i).getDamage());
+			    }
+		
+		Collections.sort(monstruos, new MonsterComparatorDamage());
+		System.out.println("---------Lista Ordenada-------------");
+		 for (int i=0;i<monstruos.size();i++) {
+		      
+		      System.out.println(monstruos.get(i).getDamage());
+		    }
+		
+		return monstruos;
+	}
+}
