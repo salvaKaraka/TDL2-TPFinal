9e65180d8aec9d0caa65d916de38edcc96ec1179
Salvador <salvador.karakachoff@gmail.com>
2023-12-18 20:36:43 -0300

    Se eliminaron 3 archivos que no eran necesarios

--- src/entregable/MonsterComparatorDamage.java
+++
@@ -1,9 +0,0 @@
-package entregable;
-
-import game.components.Monster;
-public class MonsterComparatorDamage implements java.util.Comparator<Monster> {
-	@Override
-	public int compare(Monster monsterA, Monster monsterB) {
-		return (monsterA.getDamage()-monsterB.getDamage());
-	}
-}
